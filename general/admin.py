3d2184435b966965b7132c71990956d970f5316b
Vlada Didko <slava.didko@gmail.com>
2019-05-29 18:44:38 +0300

    user profile added

--- general/admin.py
+++ general/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog.models import Category
+from users.models import Profile
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Profile)
