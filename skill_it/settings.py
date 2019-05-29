d9aff09e848b6c3663fe41a92eca0cedc4bec5b2
Oleeeg <gomiiilko@gmail.com>
2019-05-29 14:39:44 +0300

    login & logout added

--- skill_it/settings.py
+++ skill_it/settings.py
@@ -128,3 +128,8 @@ STATICFILES_DIRS = [
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- skill_it/urls.py
+++ skill_it/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from general import views as main_views
 from users import views as user_views
 
@@ -22,4 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_views.home, name='home'),
     path('register/', user_views.register, name='register'),
-]
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
