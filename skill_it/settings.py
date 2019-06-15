a77d2582e67f6b515199a55b5e9edec8605787fc
Vlada Didko <slava.didko@gmail.com>
2019-06-16 00:05:36 +0300

    updated settings

--- skill_it/settings.py
+++ skill_it/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2^vs5=f(y#2(mt4*0=uav&73j5790@ne!_$z8gqi=^1^5okwqa'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['skillitapp.herokuapp.com']
 
 
 # Application definition
