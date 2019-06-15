4eab44df76cb8132c5a04d92c3e882b8c1bd2674
Vlada Didko <slava.didko@gmail.com>
2019-06-15 23:25:15 +0300

    requirements.txt added

--- skill_it/settings.py
+++ skill_it/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'crispy_forms',
+    'responsive_images',
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'general.apps.GeneralConfig',
